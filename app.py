c4c3da7ffaf11bc6d4e8aff6099a6346fb744861
thaonh4109 <thaonh4109@gmail.com>
2026-07-21 12:14:28 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/chickens")
+@app.route("/chiikawa")
+def chickens():
+    return render_template("chickens.html")
+
+@app.route("/pandas")
+def pandas():
+    return render_template("pandas.html")
+
+@app.route("/butterflies")
+@app.route("/izna")
+def butterflies():
+    return render_template("butterflies.html")
+
+if __name__ == "__main__":
+    app.run(port="5678")
